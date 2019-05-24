3813683fa54d9dcf74f8c23d6e8124f5279a5ff0
houssin mansouri <houssin.mansouri93@gmail.com>
2019-05-24 13:50:43 +0200

    add and edit Contact

--- src/main/java/co/elmansouri/contact/controller/ContactController.java
+++ src/main/java/co/elmansouri/contact/controller/ContactController.java
@@ -3,9 +3,16 @@ package co.elmansouri.contact.controller;
 import java.util.Collections;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.elmansouri.contact.entities.Contact;
@@ -27,7 +34,10 @@ public class ContactController {
 		return "Hello platform Stagiaires are fine !!";
 	}
 	
-	
+	/**
+	 * Liste Contact
+	 * @return
+	 */
 	@GetMapping(path = "contacts")
 	public List<Contact> getAll() {
 		List<Contact> contacts = this.contactRepository.findAll();
@@ -36,6 +46,28 @@ public class ContactController {
 		} else {
 			return contacts;
 		}
+	}
+	
+	
+	/**
+	 * Ajouter contact
+	 * @param contact
+	 * @return
+	 */
+	@PostMapping(path = "contacts")
+	public ResponseEntity<Contact> add(@Valid @RequestBody Contact contact) {
+		return new ResponseEntity<Contact>(this.contactRepository.save(contact), HttpStatus.OK);
+	}
+	
+	
+	/**
+	 * Modifier Contact
+	 * @param contact
+	 * @return
+	 */
+	@PutMapping(path = "contacts")
+	public ResponseEntity<Contact> edit(@Valid @RequestBody Contact contact) {
+		return new ResponseEntity<Contact>(this.contactRepository.save(contact), HttpStatus.OK);
 	}
 	
 
